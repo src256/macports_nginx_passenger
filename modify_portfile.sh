e13d08e80dbca8873073959e2180c879523cdf92
src <src@srcw.net>
2022-02-19 15:14:20 +0900

    disable sandbox

--- modify_portfile.sh
+++ modify_portfile.sh
@@ -3,7 +3,9 @@
 portfile=/opt/local/var/macports/sources/rsync.macports.org/macports/release/tarballs/ports/www/nginx/Portfile
 passengerdir=$(passenger-config --root)
 
+#set portsandbox_active no
 str="
+
 variant passenger description {Adds passenger support} {
     configure.args-append   --add-module=$passengerdir/src/nginx_module
 }
